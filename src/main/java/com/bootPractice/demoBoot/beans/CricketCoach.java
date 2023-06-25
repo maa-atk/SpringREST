acc5391cfa5f475d3815ba3b3bc423e830cee027
maa-atk <maa.abhitk@gmail.com>
2023-06-25 15:01:25 +0530

    Bean-LifeCycle @PostConstruct after constructor @PreDestroy run just before the app is closed *Prototype bean is lazy and does not call back destroy method

--- src/main/java/com/bootPractice/demoBoot/beans/CricketCoach.java
+++ src/main/java/com/bootPractice/demoBoot/beans/CricketCoach.java
@@ -1,14 +1,24 @@
 package com.bootPractice.demoBoot.beans;
 
 import com.bootPractice.demoBoot.interfaces.Coach;
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CricketCoach implements Coach {
+
     public CricketCoach() {
         System.out.println("CricketCoach Constr");
     }
-
+@PostConstruct
+        public void startUpMethod(){
+        System.out.println("after creation of bean");
+}
+    @PreDestroy
+    public void beforeDestroyMethod(){
+        System.out.println("for bean clean up after stop application");
+    }
     @Override
     public String getDailyWorkout() {
         return "Practice Bowl 10min ";
