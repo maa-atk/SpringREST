bbf6b73725289c9623cbcff93584f1d07fb257ae
maa-atk <maa.abhitk@gmail.com>
2023-06-27 12:16:39 +0530

    setup2

--- src/main/java/com/atk/curddemo/CurddemoApplication.java
+++ src/main/java/com/atk/curddemo/CurddemoApplication.java
@@ -1,7 +1,9 @@
 package com.atk.curddemo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CurddemoApplication {
@@ -9,5 +11,11 @@ public class CurddemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CurddemoApplication.class, args);
 	}
-
+	//creating a cmd based app will be run after all the beans are created
+	@Bean
+	public CommandLineRunner commandLineRunner (String[] args){
+		return runner->{
+			System.out.println("App start");
+		};
+		}
 }
